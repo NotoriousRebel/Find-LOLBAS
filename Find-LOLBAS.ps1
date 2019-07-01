9b98a0d58ea72f24b2969bf5a024f0e72c70015f
NotoriousRebel <matt12299@yahoo.com>
2019-06-30 22:16:01 -0400

    Initial commit, built hashmap mapping path to array of name and command

---
+++ Find-LOLBAS.ps1
@@ -0,0 +1,20 @@
+function Find-LOLBAS{
+<#
+.SYNOPSIS
+Script which can be used to find living off land binaries and scripts on a target machine.
+
+.DESCRIPTION
+The script searches through known locations of Living off Land Binaries and Scripts
+and identifies if they exist. In the case they do exist it will output the name of the binary or
+script, the full path, and how to use it.
+
+.EXAMPLE
+PS C:\> Find-LOLBAS
+
+.LINK
+https://github.com/LOLBAS-Project/LOLBAS
+#>
+
+
+
+}
